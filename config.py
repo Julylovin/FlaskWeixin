8f42616e9c7e352bdb1d5248086b581547aac691
TangKang <julylovin@163.com>
2025-03-19 15:58:53 +0800

    --

---
+++ config.py
@@ -0,0 +1,4 @@
+QINIU_ACCESS_KEY = 'faGfOLwwUfuAORfy3O1ioEEzLLrrBQnAlj2z83DF'
+QINIU_SECRET_KEY = 'uk6olitpFonNK7arjlwgNWc-mL1M4cTIDD8AGvgh'
+QINIU_BUCKET_NAME = 'mianhuatangss'
+QINIU_DOMAIN = 'https://d.biaoguanhealth.com/'
