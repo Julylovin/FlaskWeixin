d926ee97a555388ef907474f0849f6d908fa79bf
Julylovin <julylovin@163.com>
2025-05-07 12:57:04 +0800

    --

--- config.py
+++
@@ -1,4 +0,0 @@
-QINIU_ACCESS_KEY = 'faGfOLwwUfuAORfy3O1ioEEzLLrrBQnAlj2z83DF'
-QINIU_SECRET_KEY = 'uk6olitpFonNK7arjlwgNWc-mL1M4cTIDD8AGvgh'
-QINIU_BUCKET_NAME = 'mianhuatangss'
-QINIU_DOMAIN = 'https://d.biaoguanhealth.com/'
